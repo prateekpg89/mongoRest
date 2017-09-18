cfad82855c45e854ba565ac31f79a48c78407143
prateek89adobe <praverma@adobe.com>
2017-09-19 03:04:10 +0530

    Committing changes to support CORS for all HTTP methods. Added support to PUT for play/pause

--- routes/steps.js
+++ routes/steps.js
@@ -2,7 +2,7 @@ var express     =   require("express");
 var successResp =   require("../response/success");
 var errorResp   =   require("../response/error");
 var router      =   express.Router();
-var mongoCon     =   require("./db-connection");
+var mongoCon     =   require("../utils/db-connection");
 // create schema
 var userSchema  = {
     "steps" : [{
@@ -21,9 +21,22 @@ var mongoOp = mongoCon.model('StepUI',userSchema, 'StepUI');
 router.get("/",function(req,res){
     mongoOp.find({},{'steps':1, '_id':0},(err, data)=>{
         if(err){
-            res.json(errorResp("Issues with connection to database", 401));
+            res.json(errorResp("Something went wrong!!", 401));
+        }else if(!data){
+            res.json(errorResp("No records found", 404));
         }else{
-            console.log(data);
+            res.json(successResp(data));
+        }
+    });
+});
+
+router.put("/:stepid",function(req,res){
+    mongoOp.findOneAndUpdate({"steps.stepId": req.params.stepid},{"steps.$.stepStatus":req.body.flag},(err, data)=>{
+        if(err){
+            res.json(errorResp("Something went wrong!!", 401));
+        }else if(!data){
+            res.json(errorResp("No records found for [id:"+req.params.stepid+"]", 404));
+        }else{
             res.json(successResp(data));
         }
     });
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express     =   require("express");
 var app         =   express();
 var bodyParser  =   require("body-parser");
-var steps       =   require("./models/steps");
+var steps       =   require("./routes/steps");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS, PATCH');// to send this header as response to preflight 
+    //request when browser calls OPTIONS tocheck what type of HTTP method back supports otherwise methods which involve write to
+    //HTTP resource will not work due to CORS.
     next();
 });
 
